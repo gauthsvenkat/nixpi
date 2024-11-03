29248b2ed75be99aed42829ae3cdb0f3da07db3b
Gautham <gauthsvenkat@gmail.com>
2024-11-03 21:08:48 +0100

    bootstrapped

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,45 @@
-{config, pkgs, nixpkgs, ...}: {
+{
+  config,
+  pkgs,
+  nixpkgs,
+  ...
+}:
+{
   system.stateVersion = "24.05"; # Don't change this unless you know what you're doing
-  
+
   imports = [
     "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
   ];
 
-  # age.secrets = {
-  #   "nixpi-hashed-password.age".file = ./secrets/nixpi-hashed-password.age;
-  #   "wireless.conf".file = ./secrets/wireless.conf;
-  # };
+  age.secrets = {
+    "nixpi-hashed-password.age".file = ./secrets/nixpi-hashed-password.age;
+    "wireless.conf".file = ./secrets/wireless.conf;
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
-    options = ["noatime"];
+    options = [ "noatime" ];
   };
 
   networking = {
     hostName = "nixpi";
     wireless = {
       enable = true;
-      interfaces = ["wlan0"];
-      # secretsFile = config.age.secrets."wireless.conf".path;
-      # networks."nsa_surveillance".pskRaw = "ext:password";
-      networks."nsa_surveillance".psk = "maythebloodonyourswordneverdry";
+      interfaces = [ "wlan0" ];
+      # Use following for first time setup
+      # networks."nsa_surveillance".psk = "<ENTER-PASSWORD-FIRST-TIME>";
+      secretsFile = config.age.secrets."wireless.conf".path;
+      networks."nsa_surveillance".pskRaw = "ext:password";
     };
   };
 
   services.openssh.enable = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   environment.systemPackages = with pkgs; [
     neovim
@@ -39,10 +49,12 @@
     mutableUsers = false;
     users."ando" = {
       isNormalUser = true;
-      # hashedPasswordFile = config.age.secrets."nixpi-hashed-password.age".path;
-      password = "1806";
-      extraGroups = ["wheel"];
+      # Use following for first time setup
+      # password = "<USER-PASSWORD>";
+      hashedPasswordFile = config.age.secrets."nixpi-hashed-password.age".path;
+      extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [
+        # Use whatever ssh pub key at the time of building image 
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjZq6GCEU+TpzLRthwvjzN6pPO+gJt2ngakYpxycf+y ando@thunderdome"
       ];
       packages = with pkgs; [
---
+++ secrets/secrets.nix
@@ -0,0 +1,13 @@
+let
+  ando_thunderdome = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjZq6GCEU+TpzLRthwvjzN6pPO+gJt2ngakYpxycf+y";
+  root_nixpi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOEqqDVfoH+P9saryI34gsYrZXwtWF/oJKFZcUKs0W8p";
+
+  public_keys = [
+    ando_thunderdome
+    root_nixpi
+  ];
+in
+{
+  "nixpi-hashed-password.age".publicKeys = public_keys;
+  "wireless.conf".publicKeys = public_keys;
+}
